034f8e11883de74f81ee0133bee43b6cfb041064
mikhailstoyanov <gmishak@gmail.com>
2019-01-25 15:45:23 +0200

    Add CreateNotifications command

--- src/NotificationServiceProvider.php
+++ src/NotificationServiceProvider.php
@@ -30,7 +30,7 @@ class NotificationServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->commands([
-                Oxygencms\Notifications\Commands\CreateNotifications::class, 
+                Commands\CreateNotifications::class, 
             ]);
         }
     }
